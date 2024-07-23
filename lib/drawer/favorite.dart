7e1e525ab00d2a098ab50437b140edfd29c7eeba
Vidhyadhar Reddy kudumula <kudumala123@gmail.com>
2024-07-23 06:26:00 +0530

    liked page done

--- lib/drawer/favorite.dart
+++ lib/drawer/favorite.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sporty/uicomponents/cards.dart'; // Adjust the import according to your project structure
 
 class FavoriteScreen extends StatefulWidget {
@@ -22,30 +24,34 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text('Favorites'),
-        titleTextStyle: const TextStyle(
-          color: Colors.green,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: favoritesList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(favoritesList[index].name), // Adjust according to your SportsField model
-            // Add other properties of SportsField as needed
-          );
-        },
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(
+              icon: const Icon(Icons.arrow_back_ios, color: Colors.green, size: 24,),
+              onPressed: () => Navigator.pop(context), 
+              ),
+              SizedBox(width: MediaQuery.of(context).size.width * 0.3,),
+              const Text("Liked",
+              style: TextStyle(color: Colors.green,
+              fontSize: 24,
+              decoration: TextDecoration.none,),)
+            ],
+          ),
+          Expanded( // Added Expanded to allow ListView to take available space
+            child: ListView.builder(
+              itemCount: favoritesList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(favoritesList[index].name), // Adjust according to your SportsField model
+                  // Add other properties of SportsField as needed
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
