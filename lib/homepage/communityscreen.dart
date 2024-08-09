dd34f6e89408ad2842b2ae01fb0a8721d7cb801c
HarshaVardhan2k04 <21r01a67b2@cmritonline.ac.in>
2024-08-09 17:53:13 +0530

    er

--- lib/homepage/communityscreen.dart
+++ lib/homepage/communityscreen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:sporty/homepage/testpage.dart';
 import 'package:sporty/uicomponents/elements.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';  
+// import 'package:sporty/homepage/testpage.dart';
 
 class CommunityScreen extends StatefulWidget {
   const CommunityScreen({super.key});
